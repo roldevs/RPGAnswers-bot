92dae672be8b0075bcc4f3ff64857971462aa51a
Alberaan <43955907+Alberaan@users.noreply.github.com>
2018-11-03 14:59:47 +0100

    Bot only answers his new commands

--- botlogic.py
+++ botlogic.py
@@ -5,14 +5,14 @@ def processCommand(command):
 
     response = ""
 
-    if command.startswith("/list"):
+    if command.startswith("/rpglist"):
         response = listRequest(command) 
-    elif command.startswith("/gen"):
+    elif command.startswith("/rpggen"):
         response = genRequest(command)
     elif command.startswith("/help"):
         response = helpRequest()
-    elif command.startswith("/"):
-        response = helpRequest()
+    else:
+        pass
 
     return response
 
@@ -21,8 +21,8 @@ def helpRequest():
 	text = ""
 	
 	text += "Available commands" + "\n"
-	text += "/list [lang][system]: this command lists the available languages, the available systems per language and the available random tables per systme" + "\n"
-	text += "/gen [lang][system][table]: this command generates a result from the defined language, system and table" + "\n"
+	text += "/rpglist [lang][system]: this command lists the available languages, the available systems per language and the available random tables per systme" + "\n"
+	text += "/rpggen [lang][system][table]: this command generates a result from the defined language, system and table" + "\n"
 	text += "/help: lists this menu" + "\n"
 
 	return text
@@ -31,7 +31,7 @@ def listRequest(msg):
     command = msg
     basicurl = "https://hall.herokuapp.com/api/types"
 
-    arguments = command.replace("/list", "")
+    arguments = command.replace("/rpglist", "")
     options = arguments.split(" ")
     options.pop(0)
 
@@ -76,7 +76,7 @@ def genRequest(msg):
     command = msg
     basicurl = "https://hall.herokuapp.com/api/random"
 
-    arguments = command.replace("/gen", "")
+    arguments = command.replace("/rpggen", "")
     options = arguments.split(" ")
     options.pop(0)
 
