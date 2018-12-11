65ec111e732cbafdf9a948f698ed4b08f43995be
Alberaan <alberaan@gmail.com>
2018-12-11 09:52:47 +0100

    fised ifs in discord

--- rpganswersdiscord.py
+++ rpganswersdiscord.py
@@ -27,21 +27,24 @@ async def on_message(message):
         await send_message(message.channel, response)
 
 async def send_message(channel, response):
-    if isinstance(response, botresponse):
-        text = response.header + "\n"
-        for textLine in response.lines:
-            if textLine.lineType== "normal":
-                text += textLine.text + "\n"
-            if textLine.lineType == "table":
-                text += indent(textLine.indent) + "[" + textLine.text + "]" + "\n"
-            if textLine.lineType == "attribute":
-                text += indent(textLine.indent) + textLine.attribute + ": " + textLine.attributeValue + "\n"
+    if isinstance(response, botresponse) == False:
+        return
 
-        await client.send_message(channel, text)
+    text = response.header + "\n"
+    for textLine in response.lines:
+        if textLine.lineType== "normal":
+            text += textLine.text + "\n"
+        if textLine.lineType == "table":
+            text += indent(textLine.indent) + "[" + textLine.text + "]" + "\n"
+        if textLine.lineType == "attribute":
+            text += indent(textLine.indent) + textLine.attribute + ": " + textLine.attributeValue + "\n"
+
+    await client.send_message(channel, text)
 
 
 # Main starts here
 
-token = str(os.environ["discord_token"])
+#token = str(os.environ["discord_token"])
+token = "NTA3MjE0NzgwODEwMDAyNDYz.DvELVw.73AhPsiAKqghU9YCewGAV7LSLWQ"
 
 client.run(token)
